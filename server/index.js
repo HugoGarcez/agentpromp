bdf4d5541879ff142eb1ef0d2eb0d4bffd252c4c
hugogarcez <hugocgarcez@gmail.com>
2026-02-06 23:28:01 -0300

    Fix: Load .env from absolute path

--- server/index.js
+++ server/index.js
@@ -14,10 +14,10 @@ import axios from 'axios';
 import FormData from 'form-data';
 
 // Load environment variables
-dotenv.config();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 const PORT = 3001;
